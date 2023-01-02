c94a75b7f965437bec211e3bb840c99974f98e24
marcoghezzi1 <marco.ghezzi3@studenti.unimi.it>
2023-01-02 10:12:25 +0100

    Added new relative path in fear.py (working?) and behavior file of subject 45

--- deepemogp/datasets/fear.py
+++ deepemogp/datasets/fear.py
@@ -6,9 +6,10 @@ from scipy import io
 
 class FEAR(Dataset):
     # path of the directory containing physiological signals
-    signal_folder = '/home/paolo/matteo/matteo/unimi/tesi_master/code/fear_gen/data/sync_signals'
+    # signal_folder = '/home/paolo/matteo/matteo/unimi/tesi_master/code/fear_gen/data/sync_signals'
 
     # signal_folder = '/Users/marcoghezzi/PycharmProjects/pythonProject/fear_gen/data/sync_signals'
+    signal_folder = 'data/sync_signals'
 
     def __init__(self, name='FEAR', signals=None, subjects=None, ):
         super(FEAR, self).__init__(name, signals, subjects, annotations=None)
