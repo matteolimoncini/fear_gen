5f30042b1cbb99c5b4ed78922343960e845ad249
matteolimoncini <matteo@limoncini.it>
2023-01-14 10:26:45 +0100

    added file for server with more k

--- hierarchical_all_subjects.py
+++ hierarchical_all_subjects.py
@@ -22,7 +22,7 @@ warnings.simplefilter(action="ignore", category=RuntimeWarning)
 scaler = StandardScaler()
 
 valid_subject = extract_correct_csv.extract_only_valid_subject()
-valid_k_list = list(range(1, 10))
+valid_k_list = list(range(10, 16))
 
 for k in valid_k_list:
 
