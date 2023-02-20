af1f4a6d1de0b698a9a142bbb452860e84d6e206
matteolimoncini <matteo@limoncini.it>
2023-02-20 09:05:28 +0100

    modified pymdp model and check gaze feat extraction

---
+++ pyro/category_learning/test.py
@@ -0,0 +1,31 @@
+import pyro
+import torch
+import pyro.distributions as dist
+import pyro.distributions.constraints as constraints
+import pandas as pd
+
+DATA_URL = "https://d2hg8soec8ck9v.cloudfront.net/datasets/rugged_data.csv"
+data = pd.read_csv(DATA_URL, encoding="ISO-8859-1")
+df = data[["cont_africa", "rugged", "rgdppc_2000"]]
+
+train = torch.tensor(df.values, dtype=torch.float)
+is_cont_africa, ruggedness, log_gdp = train[:, 0], train[:, 1], train[:, 2]
+
+train = torch.tensor(df.values, dtype=torch.float)
+is_cont_africa, ruggedness, log_gdp = train[:, 0], train[:, 1], train[:, 2]
+
+
+def simple_model(is_cont_africa, ruggedness, log_gdp=None):
+    a = pyro.param("a", lambda: torch.randn(()))
+    b_a = pyro.param("bA", lambda: torch.randn(()))
+    b_r = pyro.param("bR", lambda: torch.randn(()))
+    b_ar = pyro.param("bAR", lambda: torch.randn(()))
+    sigma = pyro.param("sigma", lambda: torch.ones(()), constraint=constraints.positive)
+
+    mean = a + b_a * is_cont_africa + b_r * ruggedness + b_ar * is_cont_africa * ruggedness
+
+    with pyro.plate("data", len(ruggedness)):
+        return pyro.sample("obs", dist.Normal(mean, sigma), obs=log_gdp)
+
+
+pyro.render_model(simple_model, model_args=(is_cont_africa, ruggedness, log_gdp), render_distributions=True)
