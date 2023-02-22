049b625c45d063e08e57e90f6ed7e0681c93be51
marcoghezzi1 <marco.ghezzi3@studenti.unimi.it>
2023-02-22 12:03:40 +0100

    Update rational agent Pyro

--- pyro/category_learning/rational_agent_pyro.py
+++ pyro/category_learning/rational_agent_pyro.py
@@ -1,6 +1,6 @@
 import logging
 
-
+import pandas as pd
 import numpy as np
 import torch
 import pyro
@@ -9,27 +9,44 @@ from pyro.infer import SVI, Trace_ELBO, MCMC, NUTS
 import pyro.distributions as dist
 
 logging.basicConfig(format='%(message)s', level=logging.INFO)
-
+# read dataset
+df_ = pd.read_csv('fear_gen/data/newLookAtMe01.csv')
+print(df_)
+df_ = df_[['morphing level', 'shock']]
+df_['shock'] = df_['shock'].astype(int)
+df_['morphing level'] = [int(d==6) for d in df_['morphing level']]
 
 # categorical/multinomial distribution
 # LDA
-prior_counts = torch.ones(4)
-
-
+prior_counts = torch.ones((2,2))
 def model(data):
     prior = pyro.sample("prior", dist.Dirichlet(prior_counts))
     total_counts = int(data.sum())
     pyro.sample("posterior", dist.Multinomial(total_counts, prior), obs=data)
 
 
-data = torch.tensor([[0, 0], [0, 0], [0, 0], [0, 0], [1, 0], [0, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1]])
-data_np = data.numpy()
+# data = torch.tensor([[0, 0], [0, 0], [0, 0], [0, 0], [1, 0],
+# [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1]])
+data_np = df_.to_numpy()
+data_np = data_np[16:]
+data = torch.tensor(data_np)
 
-counter = torch.zeros((len(data_np), 4))
+N = data.shape[0]
+
+counter = torch.zeros((N,4))
 for i in range(len(data_np)):
     tmp_array = np.unique(data_np[:i+1], axis=0, return_counts=True)[1]
     for j in range(len(tmp_array)):
-        counter[i][j] = tmp_array[j]
+        if len(tmp_array) > 1:
+            if j != 0:
+                counter[i][j+1] = tmp_array[j]
+            else:
+                counter[i][0] = tmp_array[0]
+        else:
+            counter[i][j] = tmp_array[j]
+
+counter = counter.reshape((len(data_np), 2, 2))
+
 nuts_kernel = NUTS(model)
 num_samples, warmup_steps = (1000, 200)
 mcmc = MCMC(nuts_kernel, num_samples=num_samples, warmup_steps=warmup_steps, disable_progbar=True)
@@ -40,10 +57,8 @@ for i in range(len(counter)):
                 for k, v in mcmc.get_samples().items()}
     means = hmc_samples['prior'].mean(axis=0)
     stds = hmc_samples['prior'].std(axis=0)
-    print('observation: ', data_np[i])
-    print('probabilities: ', means)
+    logging.info("observation: {}".format(data_np[i]))
+    logging.info("probabilities: {}".format(means))
     all_means.append(means)
-
-
 
 
